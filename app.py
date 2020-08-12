d7a873426a29bf7807286b4e877a05d06c78d3dc
milimiliemilie <66954866+milimiliemilie@users.noreply.github.com>
2020-08-12 21:20:24 +0900

    moviestar delete api

--- app.py
+++ app.py
@@ -23,18 +23,20 @@ def show_stars():
     # - .sort("_id", 1)
 
     stars = list(db.mystar.find({}, {'_id': 0}).sort("like", pymongo.DESCENDING))
-    # " db에서, mystar 를 찾아올(find) 건데.. _id 는 안 가져올꺼다. "
-    # 그걸 list 화 해서 [['name': 머머, 'url': 머머머], '..', ...] stars 라고 부를 거다.
-    # 애초에 db = client.dbsparta 라고 했고, client = MongoClient('localhost', 27017) 이라고 정했다는 걸 기억하자.
-    # 결국 db = MongoClient('localhost', 27017).dbsparta 라는 뜻.
+    # 풀이 :
+    # - " db에서, mystar 를 찾아올(find) 건데.. _id 는 안 가져올꺼다. "
+    # - 그걸 list 화 해서 [['name': 머머, 'url': 머머머], '..', ...] stars 라고 부를 거다.
+    # - 애초에 db = client.dbsparta 라고 했고, client = MongoClient('localhost', 27017) 이라고 정했다는 걸 기억하자.
+    # - 결국 db = MongoClient('localhost', 27017).dbsparta 라는 뜻.
 
     # 2. articles 라는 키 값으로 articles 정보 보내주기
     result = {
         'result': 'success',
-        'stars_list': stars,  # index.html 에서, " var stars_list = response['stars_list'] " 라고 하면서 db를 불러올 것이다.
+        'stars_list': stars,
+        # - index.html 에서, " var stars_list = response['stars_list'] " 라고 하면서 db를 불러올 것이다.
     }
 
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
+    # 3. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
     return jsonify(result)
 
 
@@ -65,7 +67,12 @@ def like_star():
 @app.route('/api/delete', methods=['POST'])
 def delete_star():
     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    # - POST 이기 때문에, .form 을 쓰면 됩니다.
+    name_receive = request.form['name_give']
+
     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
+    db.mystar.delete_one({'name_receive'})
+
     # 3. 성공하면 success 메시지를 반환합니다.
     return jsonify({'result': 'success', 'msg': 'delete 연결되었습니다!'})
 
